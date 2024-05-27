4d3e1b28282dbb7e7c071e88a106856a6e335095
SindhujaReddy06 <s223671346@deakin.edu.au>
2024-05-27 23:44:25 +1000

    initial commit

---
+++ src/components/Home/InfoPage/InfoPage.jsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import './InfoPage.css';
+import { FaClock, FaHeadset,FaHouseUser  } from "react-icons/fa";
+import { Link } from 'react-router-dom';
+
+const InfoPage = () => {
+    return (
+        <section className="why-us mt-5 mt-md-0">
+            <div className="container">
+
+                <div className="row">
+                    <div className="col-lg-4 d-flex align-items-stretch">
+                        <div className="content">
+                            <h3>Why Opt for Our Services?</h3>
+                            <p>
+                            Expert Physicians: Our team consists of board-certified doctors across various specialties, committed to providing the highest standard of care.   </p>
+<p>Easy Appointments: Scheduling an appointment has never been easier. Use our online booking system to find a time that works best for you.</p>
+<p>Patient-Centered Care: We listen to your concerns and work with you to create a personalized care plan.</p>
+                         
+                            <div className="text-center">
+                                <Link href="/" className="more-btn">Learn More <i className="bx bx-chevron-right"></i></Link>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-8 d-flex align-items-stretch">
+                        <div className="icon-boxes d-flex flex-column justify-content-center">
+                            <div className="row">
+                                <div className="col-xl-4 d-flex align-items-stretch">
+                                    <div className="icon-box mt-4 mt-xl-0">
+                                        <FaHouseUser className="icon"/>
+                                        <h4>Appointment</h4>
+                                        <small className='text-secondary'>24/7 Services Available</small>
+                                        <p>We are here to help you 24/7</p>
+                                    </div>
+                                </div>
+                                <div className="col-xl-4 d-flex align-items-stretch">
+                                    <div className="icon-box mt-4 mt-xl-0">
+                                        <FaHeadset className="icon"/>
+                                        <h4>Emegency Cases</h4>
+                                        <h6 className='text-secondary'>+61 XXXXXXXX</h6>
+                                        <p>Please contact us in case of emergency</p>
+                                    </div>
+                                </div>
+                                <div className="col-xl-4 d-flex align-items-stretch">
+                                    <div className="icon-box mt-4 mt-xl-0">
+                                        <FaClock className="icon"/>
+                                        <h4>Working Hours</h4>
+                                        <small className='text-secondary'>Timing schedule</small>
+                                        <ul className='list-group list-group-flush'>
+                                        <li className="list-group-item d-flex justify-content-between text-nowrap" ><p>Mon - Fri : </p> <p>8:00 - 18: 00</p></li>
+                                        <li className="list-group-item d-flex justify-content-between text-nowrap" ><p>Sat - Sun : </p> <p>10:00 - 17: 00</p></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </section>
+    )
+}
+
+export default InfoPage
